d3d20f742bc22aebc35e98e62272efeb62050dcb
Fernanda Taso <fernandataso.op@gmail.com>
2020-08-30 11:43:22 -0400

    adicionado nova logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
         background_color: `#16202c`,
         theme_color: `#16202c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logoFT.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sitemap`,
