df9a9645c67c070b0d85b112e3cc193b36549789
Fernanda Taso <fernandataso.op@gmail.com>
2020-09-09 23:56:04 -0400

    corrigido erro de export do React

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from 'gatsby'
 //import Layout from "../components/Layout"
 import SEO from "../components/seo"
-import * as S from "./404styled"
+import * as S from "../components/404/styled"
 
 
 const NotFoundPage = () => {
@@ -19,14 +19,14 @@ const NotFoundPage = () => {
       }
     `
   )
-  return (<>
-    <SEO title="404: Not found" />
+  return (
     <S.Wrapper>
+      <SEO title="404: Not found" />
       <S.AvatarWrapper fluid={avatarImage.childImageSharp.fluid} />
       <S.Title>404</S.Title>
       <p>Você entrou na nuvem errada. Meia volta, mortal.</p>
     </S.Wrapper>
-  </>)
+  )
 }
 
 export default NotFoundPage
